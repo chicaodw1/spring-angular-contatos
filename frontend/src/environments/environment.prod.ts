aa40eee2cc3aa0171d02e561caaa9eb4712b4854
Francisco Costa <franciscocosta@Mac.local>
2025-04-16 20:49:23 -0300

    test deploy

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  userPoolId: 'us-east-1_TxXmz7Uf9',
-  userPoolClientId: '4iq1q0rui295vq2ev2b399gf0f',
-  identityPoolId: 'us-east-1:a7777ec1-65e8-428f-96a5-0900d37f25e7',
-  apiUrl: 'https://api.franciscocosta.dev.br',
-};

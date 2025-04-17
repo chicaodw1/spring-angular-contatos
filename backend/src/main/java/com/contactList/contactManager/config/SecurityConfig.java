44a88b57c4544b659a82f30357ceb6f8e12e63ce
Francisco Costa <franciscocosta@Mac.local>
2025-04-16 22:57:17 -0300

    chore: deploy 5

--- backend/src/main/java/com/contactList/contactManager/config/SecurityConfig.java
+++ backend/src/main/java/com/contactList/contactManager/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/public/**").permitAll()
+                        .requestMatchers("/actuator/health").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt

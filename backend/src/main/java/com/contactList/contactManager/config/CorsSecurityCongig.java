b2b708bb316b54ee0791d2c3e4e8cc77e76b3e64
Francisco Costa <franciscocosta@Mac.local>
2025-04-16 21:30:02 -0300

    chore: cors config

---
+++ backend/src/main/java/com/contactList/contactManager/config/CorsSecurityCongig.java
@@ -0,0 +1,30 @@
+package com.contactList.contactManager.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsSecurityCongig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "https://contactmanger-2cc61.web.app",
+                "https://franciscocosta.dev.br",
+                "https://www.franciscocosta.dev.br"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
